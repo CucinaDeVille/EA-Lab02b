cd2546eab4246df44cac8db4efea63430328ce16
luca <luca.romano@t-online.de>
2026-04-17 17:15:36 +0200

    feat(button): :construction: add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { useState } from "react";
 import Timer from "./Timer.jsx"
 
 export default function App() {
 
+  const [run, setRun] = useState(true);
+
   return (
-    <Timer initCalls="10000" doRun={true}></Timer>
+      <div>
+        <Timer initCalls="10000" doRun={run} />
+
+        <button className="NiceButton" onClick={() => setRun(prev => !prev)}>
+          {String(run)}
+        </button>
+      </div>
   );
 }
