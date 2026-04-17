07596775d80bc2fc434cb3e438ae7fa9fbd3228d
luca <luca.romano@t-online.de>
2026-04-17 17:17:57 +0200

    feat(button): :poop: add functionality to button

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,10 +1,7 @@
 import './Timer.css'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function Timer({initCalls, doRun}) {
-
-    const [run] = useState(doRun);
-    const [calls] = useState(initCalls);
+function Timer({initCalls, doRun}) {
 
     function getDateAsString() {
         let d = new Date();
@@ -12,11 +9,34 @@ export default function Timer({initCalls, doRun}) {
             + d.getSeconds();
     }
 
+    const [calls, setCalls] = useState(initCalls);
+    const [time, setTime] = useState(getDateAsString());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(getDateAsString());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        if (!doRun) return;
+
+        const interval = setInterval(() => {
+            setCalls(prev => prev - 1);
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, [doRun]);
+
     return (
         <div className="App">
-            <div className="timer"> {getDateAsString()} </div>
+            <div className="timer"> {time} </div>
             <div className="timer"> {calls} </div>
-            <div className="timer"> {run ? 'ON' : 'OFF'} </div>
+            <div className="timer"> {doRun ? 'ON' : 'OFF'} </div>
         </div>
     );
 }
+
+export default Timer
