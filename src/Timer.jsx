f57b94bc490121a06287ff03ff22370885f83ba0
luca <luca.romano@t-online.de>
2026-04-17 12:08:54 +0200

    feat(timer): :sparkles: add time to topmost box - rerendered each time the page is refreshed

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -2,9 +2,15 @@ import './Timer.css'
 
 export default function Timer() {
 
+    function getDateAsString() {
+        let d = new Date();
+        return d.getHours() + ":" + d.getMinutes() + ":"
+            + d.getSeconds();
+    }
+
     return (
         <div className="App">
-            <div className="timer">Zeile 1</div>
+            <div className="timer"> {getDateAsString()} </div>
             <div className="timer">Zeile 2</div>
             <div className="timer">Zeile 3</div>
         </div>
