b433cab8c256abf9108708d2cd9313beb2d7ec24
luca <luca.romano@t-online.de>
2026-04-18 11:31:24 +0200

    feat(elements): :bulb: add explainatory comments in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
-import { useState } from "react";
+import {useState} from "react";
 import Timer from "./Timer.jsx"
 
 export default function App() {
 
-  const [run, setRun] = useState(true);
+    // define and set state of run
+    const [run, setRun] = useState(false);
 
-  return (
-      <div>
-        <Timer initCalls="10000" doRun={run} />
+    return (
+        <div>
 
-        <button className="NiceButton" onClick={() => setRun(prev => !prev)}>
-          {String(run)}
-        </button>
-      </div>
-  );
+            // call Timer and pass values as props
+            <Timer initNumber="10000" doRun={run}/>
+
+            // define button and anonymous function to change value of run
+            <button className="NiceButton" onClick={() => setRun(prev => !prev)}>
+                {String(run)}
+            </button>
+        </div>
+    );
 }
--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,17 +1,22 @@
 import './Timer.css'
 import { useState, useEffect } from "react";
 
-export default function Timer({initCalls, doRun}) {
+export default function Timer({initNumber, doRun}) {
 
+    // calculate and return time (hh:mm:ss)
     function getDateAsString() {
         let d = new Date();
         return d.getHours() + ":" + d.getMinutes() + ":"
             + d.getSeconds();
     }
 
-    const [calls, setCalls] = useState(initCalls);
+    // define and set to given value
+    const [num, setNum] = useState(initNumber);
+
+    // define and set to current time
     const [time, setTime] = useState(getDateAsString());
 
+    // get current time every second and update UI via setter method
     useEffect(() => {
         const interval = setInterval(() => {
             setTime(getDateAsString());
@@ -20,11 +25,12 @@ export default function Timer({initCalls, doRun}) {
         return () => clearInterval(interval);
     }, []);
 
+    // decrement passed variable IF doRun is true
     useEffect(() => {
         if (!doRun) return;
 
         const interval = setInterval(() => {
-            setCalls(prev => prev - 1);
+            setNum(prev => prev - 1);
         }, 100);
 
         return () => clearInterval(interval);
@@ -33,7 +39,7 @@ export default function Timer({initCalls, doRun}) {
     return (
         <div className="App">
             <div className="timer"> {time} </div>
-            <div className="timer"> {calls} </div>
+            <div className="timer"> {num} </div>
             <div className="timer"> {doRun ? 'ON' : 'OFF'} </div>
         </div>
     );
